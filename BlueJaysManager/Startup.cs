126f1cc2d17f674387645a5bfe461701935d1d80
Kevin Ma <kbma.kevin@gmail.com>
2016-10-13 18:59:12 -0400

    added Identity / OWIN Authentication - basic setup

---
+++ BlueJaysManager/Startup.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+// required for OWIN Startup
+using Microsoft.AspNet.Identity;
+using Microsoft.Owin.Security.Cookies;
+
+[assembly: OwinStartup(typeof(BlueJaysManager.Startup))]
+
+namespace BlueJaysManager
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+                LoginPath = new PathString("/Login.aspx")
+            });
+        }
+    }
+}
