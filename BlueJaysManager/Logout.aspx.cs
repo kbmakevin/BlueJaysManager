6bb96cb95397d2058464667f172331d9bab72187
Kevin Ma <kbma.kevin@gmail.com>
2016-10-13 19:30:46 -0400

    added Logout and register logic

--- BlueJaysManager/Logout.aspx.cs
+++ BlueJaysManager/Logout.aspx.cs
@@ -5,13 +5,25 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
+// required for Identity and OWIN Security
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin.Security;
+
 namespace BlueJaysManager
 {
     public partial class Logout : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            // store session info and authentication methods in the authentication manager object
+            var authenticationManager = HttpContext.Current.GetOwinContext().Authentication;
 
+            // perform signout
+            authenticationManager.SignOut();
+
+            // Redirect the user to the Login Page
+            Response.Redirect("Login.aspx");
         }
     }
 }
--- BlueJaysManager/Register.aspx.cs
+++ BlueJaysManager/Register.aspx.cs
@@ -5,6 +5,11 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
+// required for Identity and OWIN Security
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin.Security;
+
 namespace BlueJaysManager
 {
     public partial class Register : System.Web.UI.Page
@@ -16,12 +21,46 @@ namespace BlueJaysManager
 
         protected void CancelButton_Click(object sender, EventArgs e)
         {
-
+            // redirect back to the Default page
+            Response.Redirect("Default.aspx");
         }
 
         protected void RegisterButton_Click(object sender, EventArgs e)
         {
+            // create new userStore and userManager objects
+            var userStore = new UserStore<IdentityUser>();
+            var userManager = new UserManager<IdentityUser>(userStore);
 
+            // create a new user object
+            var user = new IdentityUser()
+            {
+                UserName = UserNameTextBox.Text,
+                PhoneNumber = PhoneNumberTextBox.Text,
+                Email = EmailTextBox.Text
+            };
+
+            // create a new user in the db and store the results
+            IdentityResult result = userManager.Create(user, PasswordTextBox.Text);
+
+            // check if successfully registered?
+            if (result.Succeeded)
+            {
+                // authenticate and login our new user
+                var authenticationManager = HttpContext.Current.GetOwinContext().Authentication;
+                var userIdentity = userManager.CreateIdentity(user, DefaultAuthenticationTypes.ApplicationCookie);
+
+                // sign in the user
+                authenticationManager.SignIn(new AuthenticationProperties() { }, userIdentity);
+
+                // Redirect to the Main Menu Page
+                Response.Redirect("BlueJays/MainMenu.aspx");
+            }
+            else
+            {
+                // display error in the AlertFlash div
+                StatusLabel.Text = result.Errors.FirstOrDefault();
+                AlertFlash.Visible = false;
+            }
         }
     }
 }
